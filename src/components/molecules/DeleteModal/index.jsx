63be7e3a25b4b6be7c653b70bc0a1ec528c1cf46
Neha Nandi <nandineha09@gmail.com>
2023-11-30 20:44:43 +0000

    Formatting changes

--- src/components/molecules/DeleteModal/index.jsx
+++ src/components/molecules/DeleteModal/index.jsx
@@ -1,19 +1,25 @@
-import { useEffect } from 'react'
-import PropTypes from 'prop-types'
+import { useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-import Button from '../../atoms/Button'
-import {ReactComponent as CrossIcon} from '../../../assets/cross.svg'
+import Button from '../../atoms/Button';
+import { ReactComponent as CrossIcon } from '../../../assets/cross.svg';
 
-import styles from './DeleteModal.module.scss'
+import styles from './DeleteModal.module.scss';
 
-const DeleteModal = ({ deviceName, onDeleteClick, onCancelClick }) => {
+const DeleteModal = ({
+  deviceName,
+  onDeleteClick,
+  onCancelClick
+}) => {
   useEffect(() => {
-    document.body.style.overflow = 'hidden'
-    return () => {document.body.style.overflow = 'scroll'}
-  }, [])
+    document.body.style.overflow = 'hidden';
+    return () => {
+      document.body.style.overflow = 'scroll';
+    };
+  }, []);
 
   return (
-    <div data-testid="delete-modal" className={styles.backdrop}>
+    <div data-testid='delete-modal' className={styles.backdrop}>
       <div className={styles.modal}>
         <div className={styles.modal__header}>
           <h2 className={styles.modal__title}>Delete Device?</h2>
@@ -26,7 +32,8 @@ const DeleteModal = ({ deviceName, onDeleteClick, onCancelClick }) => {
           </button>
         </div>
         <span className={styles.modal__text}>
-          You are about to delete the device <span>{deviceName}</span>. This action cannot be undone.
+          You are about to delete the device <span>{deviceName}</span>
+          . This action cannot be undone.
         </span>
         <div className={styles.modal__buttons}>
           <Button
@@ -42,13 +49,13 @@ const DeleteModal = ({ deviceName, onDeleteClick, onCancelClick }) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 DeleteModal.propTypes = {
   deviceName: PropTypes.string.isRequired,
   onDeleteClick: PropTypes.func.isRequired,
-  onCancelClick: PropTypes.func.isRequired,
-}
+  onCancelClick: PropTypes.func.isRequired
+};
 
-export default DeleteModal
+export default DeleteModal;
